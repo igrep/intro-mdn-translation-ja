26d0085b62a69eec013006c1b8c82f8119f6845e
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2024-02-11 23:50:02 +0900

    Improve the script

--- modify-github.js
+++ modify-github.js
@@ -2,22 +2,38 @@
 // @name           Modify the repository page of GitHub for screencast
 // @namespace      http://tampermonkey.net/
 // @version        2024-02-11
-// @downloadURL    https://gist.githubusercontent.com/UnicodeError0041/b5a43f278027007cc3efb6623f68358b/raw/neptun-mark-my-professor-ratings.js
-// @updateURL    https://gist.githubusercontent.com/UnicodeError0041/b5a43f278027007cc3efb6623f68358b/raw/neptun-mark-my-professor-ratings.js
+// @downloadURL    https://raw.githubusercontent.com/igrep/intro-mdn-translation-ja/main/modify-github.js
+// @updateURL      https://raw.githubusercontent.com/igrep/intro-mdn-translation-ja/main/modify-github.js
 // @description    Modify the repository page of GitHub for screencast
 // @author         igrep
-// @match        https://github.com/igrep/translated-content/*
+// @match          https://github.com/igrep/translated-content
+// @match          https://github.com/igrep/translated-content/*
 // @grant          none
 // ==/UserScript==
 
 (() => {
-  document.querySelector(".AppHeader-context-item .AppHeader-context-item-label").innerText = "mdn";
+  function exec() {
+    const appHeaderContextItemLabel = document.querySelector(".AppHeader-context-item .AppHeader-context-item-label");
+    if (appHeaderContextItemLabel !== null) {
+      appHeaderContextItemLabel.innerText = "mdn";
+    }
 
-  document.querySelector("img.avatar-user").src = "https://avatars.githubusercontent.com/u/7565578?s=48&v=4";
+    const avatarUser = document.querySelector("img.avatar-user");
+    if (avatarUser !== null) {
+      avatarUser.src = "https://avatars.githubusercontent.com/u/7565578?s=48&v=4";
+    }
 
-  const forkedFrom = document.querySelector("div.d-none");
-  forkedFrom.parentElement.removeChild(forkedFrom);
+    const forkedFrom = document.querySelector("div.d-none");
+    if (forkedFrom !== null) {
+      forkedFrom.parentElement.removeChild(forkedFrom);
+    }
 
-  const thisBranchIs = document.querySelector(".vhgA");
-  thisBranchIs.parentElement.removeChild(thisBranchIs);
+    const thisBranchIs = document.querySelector(".vhgA");
+    if (thisBranchIs !== null) {
+      thisBranchIs.parentElement.removeChild(thisBranchIs);
+    }
+  }
+  exec();
+
+  setInterval(exec, 500);
 })();
